42619fb58daeedba1f063cbd67ebe79076ea6075
K-njeru <knjeru519@gmail.com>
2025-03-26 23:58:13 +0300

    `Removed "Donate" text from Navbar donate link`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar() {
               href="/donate"
               className="text-sm font-semibold leading-6 text-white bg-transparent px-6 py-2 rounded-full shadow-lg ring-1 ring-white hover:bg-white/10 hover:shadow-xl transition-all duration-300 hover:-translate-y-1"
             >
-              Donate
+              
             </Link>
           </div>
         </div>
